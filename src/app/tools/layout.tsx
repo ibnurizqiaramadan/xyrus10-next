08a8a6b39b2868e8bd882cedf43dcfb0e6e08925
ibnurizqiaramadan <dadanibnu61@gmail.com>
2023-12-17 12:15:49 +0700

    Initial commit from Create Next App

---
+++ src/app/tools/layout.tsx
@@ -0,0 +1,11 @@
+export default function ToolLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
---
+++ src/app/tools/page.tsx
@@ -0,0 +1,10 @@
+export default function ToolsPage() {
+  return (
+    <>
+      <title>Tools</title>
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        <h1>Tools</h1>
+      </main>
+    </>
+  )
+}
