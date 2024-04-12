345ffa52ae10eeafb01b7610274bfd13444817fb
ibnurizqiaramadan <dadanibnu61@gmail.com>
2024-04-12 20:50:56 +0700

    chore: move type.d into helper

--- src/helper/type.d.ts
+++ src/helper/type.d.ts
@@ -1,3 +1,29 @@
+export type CpuData = {
+  name: string,
+  usage: number,
+  threads: number,
+  speed: number,
+  uptime: string,
+  cores: Number[]
+  coresAll: Number[]
+}
+
+export type MemoryStorage = {
+  disks: {
+    device: string;
+    used: number;
+    total: number;
+    free: number;
+    percent: number;
+  }[];
+  memPercent: number;
+  memTotal: number;
+  memUsed: number;
+  swapPercent: number;
+  swapTotal: number;
+  swapUsed: number;
+}
+
 type Format = {
   asr: number;
   filesize: number | null;
