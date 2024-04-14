1651952a093db6ce934a5195594a7471dd2281a9
ibnurizqiaramadan <dadanibnu61@gmail.com>
2024-04-15 01:33:09 +0700

    refactor: change some variable name and reducer name

--- src/features/monitor/tabSelectedSlice.ts
+++ src/features/monitor/tabSelectedSlice.ts
@@ -1,20 +1,20 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const counterSlice = createSlice({
+export const monitorTabSelectedSlice = createSlice({
   name: 'Selected Tabs Monitor',
   initialState: {
     value: '',
   },
   reducers: {
-    set(state, action) {
+    setSelectedTab(state, action) {
       state.value = action.payload;
     },
-    change: (state, action) => {
+    changeSelectedTab: (state, action) => {
       state.value = action.payload === 'chart-overall' ? 'chart-percore' : 'chart-overall';
     },
   },
 });
 
-export const { set, change } = counterSlice.actions;
+export const { setSelectedTab, changeSelectedTab } = monitorTabSelectedSlice.actions;
 
-export default counterSlice.reducer;
+export default monitorTabSelectedSlice.reducer;
